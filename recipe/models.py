4a37279072936bd659295e80964f296fc272a6ea
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-16 14:52:45 +0100

    amended models, trial and error

--- recipe/models.py
+++ recipe/models.py
@@ -38,27 +38,29 @@ class Step(models.Model):
     def __str__(self):
         return self.text
 
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=120)
 
-        def __str__(self):
-            return self.text
+    def __str__(self):
+        return self.text
 
 class Meal(models.Model):
     name = models.CharField(max_length=60)
     intro = models.CharField(max_length=180)
-    image = models.ImageField(upload_to = 'meal_images/')
+    image = models.ImageField(upload_to='meal_images/', default='meal_images/no_image.jpeg')
     preparation_time = models.IntegerField()
     cooking_time = models.IntegerField()
     user_rating = models.IntegerField(default=0)
     category = models.ManyToManyField(Category)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, default='')
-    step = models.ForeignKey(Step, on_delete=models.CASCADE, default='')
+    step = models.ManyToManyField(Step)
     ingredient = models.ManyToManyField(Ingredient)
+    comment = models.ManyToManyField(Comment)
 
     def __str__(self):
         return self.name
     
     def total_time(self):
         return self.cooking_time + self.preparation_time
+
