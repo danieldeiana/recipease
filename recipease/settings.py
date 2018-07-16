5ad07f9967f156b3f9f315a612266ed599093646
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-16 14:13:56 +0100

    registered recipe app to settings

--- recipease/settings.py
+++ recipease/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipe.apps.RecipeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
