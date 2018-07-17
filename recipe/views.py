90356ceb81767655c8a71235124acb96e3690ec0
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-17 09:53:01 +0100

    Updated index view with template

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Meal
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the index view")
+    meals = Meal.objects.all()
+    context = {'meals': meals,}
+    return render(request, 'recipe/index.html', context)
 
 def detail(request, meal_id):
     return HttpResponse("This is the detail view with meal id: {}".format(meal_id))
