0bff98e058bd157df9f8b675f866b8ff1c090830
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-17 09:26:12 +0100

    created the index and detail view, included recipe urls in recipease/urls.py

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("This is the index view")
+
+def detail(request, meal_id):
+    return HttpResponse("This is the detail view with meal id: {}".format(meal_id))
