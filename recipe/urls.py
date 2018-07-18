2e8f4efa761781f842b23ad6a89a8488cf81c5fa
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-18 09:20:06 +0100

    namespaced app urls, display login, logout and register depending on user authentication

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipe'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:meal_id>/', views.detail, name='detail'),
--- useraccount/views.py
+++ useraccount/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as user_login
+from django.contrib.auth import logout as user_logout
+from django.contrib.auth.forms import UserCreationForm 
 from django.shortcuts import redirect, render
+from .forms import LoginForm
 
 # *** remove once implemented logout view ***
 from django.http import HttpResponse
@@ -10,15 +13,29 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('recipe:index')
     else:
         form = UserCreationForm()
-    context = {'from': form}
+    context = {'form': form}
     return render(request, 'useraccount/register.html', context)
 
 def login(request):
-    return render(request, 'useraccount/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                user_login(request, user)
+                return redirect('recipe:index')
+            else:
+                return redirect('useraccount:login')
+    else:
+        form = LoginForm()
+    context = {'form': form}
+    return render(request, 'useraccount/login.html', context)
 
 def logout(request):
-    logout(request)
-    return redirect('index')
+    user_logout(request)
+    return redirect('recipe:index')
